f697d37bdf1a8c8334bf07a91ac1ad7f3c2a5e96
sabatms <110444941+sabatms@users.noreply.github.com>
2024-11-20 19:59:43 +0330

    add CourseList.jsx file

--- react-app1/src/CourseList.jsx
+++ react-app1/src/CourseList.jsx
@@ -1,12 +1,19 @@
+import CourseCard from "./CourseCard";
+
 function CourseList(){
-    const courses = ["React" , "javascript" ,"Next" , "Node"];
+    const courses = [
+        {id:1 , name: "React", description:"This is React Course"},
+        {id:2 , name : "Js" , description:"This is Js Course"},
+        {id:3 , name: "Next" , description:"This is Next Course"},
+        {id:4 , name : "Node" , description:"This is Node Course"},
+    ];
 
     return(
         <div>
             <h3>CourseList</h3>
            <ul>
-            {courses.map((course,index)=>
-                (<li key={index}>{course}</li>
+            {courses.map((course)=>
+                ( <CourseCard key={course.id} data={course}/>
                 ))}
            </ul>
         </div>
